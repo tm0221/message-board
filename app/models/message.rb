e61df816d4c7dc8c3b0d1f8f944723525cb90599
tm0221 <tomoya.matsuura@gmail.com>
2015-10-31 03:51:24 +0000

    submit.1 message-board

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20 } , presence: true
     validates :body , length: { minimum: 2, maximum: 30 } , presence: true
-    validates :age , length: { maximum: 3 } , presence: true
+    validates :age , length: { minimum: 0 } , presence: true , numericality: true
 end
